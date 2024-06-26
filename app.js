4791fc677b231216c91df38d5bb521b234c94d65
Felipe Dudek <82465554+Felipe-Dudek@users.noreply.github.com>
2024-06-26 09:47:50 -0300

    Update app.js
    Adicionando a lógica de iniciar Game

--- app.js
+++ app.js
@@ -2,6 +2,30 @@ import Player from './js/Player.js';
 import Game from './js/Game.js';
 import UI from './js/UI.js';
 
-const mainIU = new UI();
+function startGame(){
+    const player1Name = document.getElementById('player1');
+    const player2Name = document.getElementById('player2');
+    
+    ui.clearDOM();
 
-mainIU.initialScreen();
+    player1 = new Player(player1Name.value, 'X');
+    player2 = new Player(player2Name.value, 'O');
+    game = new Game(player1, player2);
+
+    ui.startGame(player1.score, player2.score, player1.playerName);
+}
+
+function cellCLick(id){
+    game.clickCell(id);
+    ui.addSimble(id);
+}
+
+let player1;
+let player2;
+let game;
+const ui = new UI();
+
+ui.initialScreen();
+
+window.startGame = startGame;
+window.cellCLick = cellCLick;
