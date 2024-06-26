0557733ff8290cf15f4b01bceffb0fd4c90ecade
Felipe Dudek <82465554+Felipe-Dudek@users.noreply.github.com>
2024-06-26 09:50:52 -0300

    Update Game.js
    Criando classe de game onde terá as informações do jogo, além de uma lógica que permite testar se o click está funcionando nas celulas

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,11 @@
 export default class Game {
-    constructor(){
-        console.log("Game Created")
+    constructor(player1, player2){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = Array(9).fill(null);
+    }
+
+    clickCell(cell){
+        alert(`clickou na célula ${cell}`)
     }
 }
