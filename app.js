1da446227c56648ef770c3ee5f9bf2b0a1fc87ef
João Vitor Pereira <91097937+pereirajaov@users.noreply.github.com>
2024-06-27 21:24:30 -0300

    Update app.js
    tcholas

--- app.js
+++ app.js
@@ -31,6 +31,30 @@ function continueGame(){
     ui.changeScore(player1.score, player2.score, player1.playerName, player2.playerName);
 }
 
+function cellClick(id){
+    const erroCellChecked = game.checkCellClick(id, player1.timeToPlay ? player1.simble : player2.simble);
+    if(erroCellChecked){
+        if(game.drawGame()){
+            ui.winGame("Tied");
+        }
+        if(game.gameOver()){
+            ui.winGame(player1.timeToPlay ? player1.playerName : player2.playerName);
+            if(player1.timeToPlay){
+                player1.score++;
+                saveLocalStorage(player1.playerName, player1.score);
+            }else{
+                player2.score++;
+                saveLocalStorage(player2.playerName, player2.score);
+            }
+        }
+        player1.timeToPlay = !player1.timeToPlay;
+        player2.timeToPlay = !player2.timeToPlay;
+        ui.changePlayer(player1.timeToPlay ? player1.playerName : player2.playerName);
+        ui.changeScore(player1.score, player2.score, player1.playerName, player2.playerName);
+        ui.addSimble(id, player1.timeToPlay ? player1.simble : player2.simble);
+    }
+}
+
 let player1;
 let player2;
 let game;
