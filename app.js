8fd96326b1b8297e4cc1d68e8fbc96d5c7d4b97c
João Vitor Pereira <91097937+pereirajaov@users.noreply.github.com>
2024-06-27 21:02:46 -0300

    Update app.js
     miau

--- app.js
+++ app.js
@@ -5,27 +5,28 @@ import UI from './js/UI.js';
 function startGame(){
     const player1Name = document.getElementById('player1');
     const player2Name = document.getElementById('player2');
-    
+
     ui.clearDOM();
 
     player1 = new Player(player1Name.value, 'X');
     player2 = new Player(player2Name.value, 'O');
-    game = new Game(player1, player2);
+    game = new Game();
 
-    ui.startGame(player1.score, player2.score, player1.playerName);
+    continueGame();
 }
 
-function cellCLick(id){
-    game.clickCell(id);
-    ui.addSimble(id);
+function continueGame(){
+    game.resetGame();
+    player1.timeToPlay = false;
+    player2.timeToPlay = false;
+    ui.clearDOM();
+    ui.startGame();
+    if(Math.random() <= 0.5){
+        player1.timeToPlay = true;
+    }
+    else{
+        player2.timeToPlay = true;
+    }
+    ui.changePlayer(player1.timeToPlay ? player1.playerName : player2.playerName);
+    ui.changeScore(player1.score, player2.score, player1.playerName, player2.playerName);
 }
-
-let player1;
-let player2;
-let game;
-const ui = new UI();
-
-ui.initialScreen();
-
-window.startGame = startGame;
-window.cellCLick = cellCLick;
