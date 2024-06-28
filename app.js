85803f4e6e36a9ef38eea94634977fe9506e09a5
Felipe Dudek <82465554+Felipe-Dudek@users.noreply.github.com>
2024-06-27 22:16:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -28,49 +28,67 @@ function continueGame(){
         player2.timeToPlay = true;
     }
     ui.changePlayer(player1.timeToPlay ? player1.playerName : player2.playerName);
-    ui.changeScore(player1.score, player2.score, player1.playerName, player2.playerName);
+    ui.changeScore(player1.win, player2.win, player1.playerName, player2.playerName);
 }
 
 function cellClick(id){
     const erroCellChecked = game.checkCellClick(id, player1.timeToPlay ? player1.simble : player2.simble);
     if(erroCellChecked){
-        if(game.drawGame()){
-            ui.winGame("Tied");
-        }
         if(game.gameOver()){
             ui.winGame(player1.timeToPlay ? player1.playerName : player2.playerName);
             if(player1.timeToPlay){
-                player1.score++;
-                saveLocalStorage(player1.playerName, player1.score);
+                player1.win++;
+                player2.lose++;
+                saveLocalStorage(player1, 'w');
+                saveLocalStorage(player2, 'l');
             }else{
-                player2.score++;
-                saveLocalStorage(player2.playerName, player2.score);
+                player2.win++;
+                player1.lose++;
+                saveLocalStorage(player2, 'w');
+                saveLocalStorage(player1, 'l');
+            }
+        } else {
+            if(game.drawGame()){
+                ui.winGame("Tied");
+                player1.Tie++;
+                player2.Tie++;
+                saveLocalStorage(player1, 't');
+                saveLocalStorage(player2, 't');
             }
         }
+        
         player1.timeToPlay = !player1.timeToPlay;
         player2.timeToPlay = !player2.timeToPlay;
         ui.changePlayer(player1.timeToPlay ? player1.playerName : player2.playerName);
-        ui.changeScore(player1.score, player2.score, player1.playerName, player2.playerName);
+        ui.changeScore(player1.win, player2.win, player1.playerName, player2.playerName);
         ui.addSimble(id, player1.timeToPlay ? player1.simble : player2.simble);   
     }
 }
 
-function saveLocalStorage(name, score){
+function saveLocalStorage(playerSave, typeScore){
     const localData = localStorage.getItem('players');
     if (localData === null) {
-        const players = [{ name: name, score: score }];
+        const players = [{ name: playerSave.playerName, win: playerSave.win, tie: playerSave.tie, lose: playerSave.lose}];
         localStorage.setItem('players', JSON.stringify(players));
     } else {
         const players = JSON.parse(localData);
         let playerFound = false;
         players.forEach(player => {
-            if (player.name === name) {
-                player.score += 1;
+            if (player.name === playerSave.playerName) {
+                if(typeScore === 'w'){
+                    player.win++;
+                }
+                else if(typeScore === 't'){
+                    player.tie++;
+                } 
+                else if(typeScore === 'l'){
+                    player.lose++;
+                }
                 playerFound = true;
             }
         });
         if (!playerFound) {
-            players.push({ name: name, score: score });
+            players.push({ name: playerSave.playerName, win: playerSave.win, tie: playerSave.tie, lose: playerSave.lose});
         }
         localStorage.setItem('players', JSON.stringify(players));
     }
@@ -79,7 +97,7 @@ function saveLocalStorage(name, score){
 function getLocalStorage(){
     const localData = localStorage.getItem('players');
     if(localData === null){
-        ui.sideMenu();
+        ui.sideMenu(null);
     } else {
         const players = JSON.parse(localData);
         ui.sideMenu(players);
