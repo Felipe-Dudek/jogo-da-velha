5ef8341634b4e5f3a27ca0596f35d69c2e0f0971
Guiblitzkow <165606191+Guiblitzkow@users.noreply.github.com>
2024-06-27 21:25:58 -0300

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -8,6 +8,34 @@ export default class Game {
         this.board = Array(9).fill(null);
         this.isGameOver = false;
     }
+    checkWin(){
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    drawGame() {
+        return this.board.every(cell => cell !== null);
+    }
+
+    gameOver(){
+        return this.isGameOver;
+    }
 
 checkCellClick(cell, simble){
         let erro = false;
